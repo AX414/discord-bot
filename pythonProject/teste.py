19ebdf41a0cea91400863f53bc34887127b6e50a
João Porcel <68911216+AX414@users.noreply.github.com>
2023-03-27 22:16:21 -0300

    Add files via upload
    Testes com o uso da API de DBD do tricky

---
+++ pythonProject/teste.py
@@ -0,0 +1,28 @@
+import requests
+import json
+import random
+
+url = "https://dbd.tricky.lol/api/"
+
+r1 = requests.get(url+"characterinfo?character="+random.choice(nomes))
+r2 = requests.get(url+"randomperks?(role=survivor&pretty)/")
+
+print(f"Response R1: {r1.json()}\n\n")
+
+# Pegar indice do personagem
+print(f"Index: {r1.json()['index']}")
+
+# Pegar nome do personagem
+print(f"Nome: {r1.json()['name']}")
+
+# Pegando as perks randomicas
+print(f"\n\nResponse R2: {r2.json()}\n\n")
+
+
+print("\n\n\n")
+data = r2.json()
+dictionary = data
+for value in dictionary.values():
+    prkn = value['name']
+    prki = value['image']
+    print(p1n+"\n"+p1i)
